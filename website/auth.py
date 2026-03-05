7e43e85bb955a5b3231e924bfa43f29aab36a640
fabiane9258 <fabiane9258@gmail.com>
2026-03-05 14:05:40 +0300

    feat: add password validation and requirements display in sign-up process

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,31 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import (
+    Blueprint, render_template, request, flash,
+    redirect, url_for
+)
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from flask_login import login_user, login_required, logout_user, current_user
+import re
 
-auth = Blueprint ('auth', __name__)
+auth = Blueprint('auth', __name__)
+
+
+def validate_password(pw: str) -> list[str]:
+    """Return list of unmet password requirements (empty if OK)."""
+    errors = []
+    if len(pw) < 8:
+        errors.append("at least 8 characters")
+    if not re.search(r"[A-Z]", pw):
+        errors.append("an uppercase letter")
+    if not re.search(r"[a-z]", pw):
+        errors.append("a lowercase letter")
+    if not re.search(r"\d", pw):
+        errors.append("a digit")
+    if not re.search(r"[!@#$%^&*(),.?\":{}|<>]", pw):
+        errors.append("a special character")
+    return errors
+
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -29,11 +50,13 @@ def login():
     print(data)
     return render_template("login.html", user=current_user)
 
+
 @auth.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('auth.login'))
+
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
@@ -53,14 +76,23 @@ def sign_up():
             flash('First name must be greater than 1 character.', category='error')
         elif not password1 or not password2 or password1 != password2:
             flash('Passwords don\'t match.', category='error')
-        elif len(password1) < 7:
-            flash('Password must be at least 7 characters.', category='error')
         else:
-            new_user = User(email=email, first_name=firstName, password=generate_password_hash(password1, method='pbkdf2'))
-            db.session.add(new_user)
-            db.session.commit()
-            login_user(user, remember=True)
-            flash('Account created!', category='success')
-            return redirect(url_for('views.home'))
+            problems = validate_password(password1)
+            if problems:
+                flash(
+                    'Password must contain ' + ', '.join(problems) + '.',
+                    category='error'
+                )
+            else:
+                new_user = User(
+                    email=email,
+                    first_name=firstName,
+                    password=generate_password_hash(password1, method='pbkdf2')
+                )
+                db.session.add(new_user)
+                db.session.commit()
+                login_user(new_user, remember=True)
+                flash('Account created!', category='success')
+                return redirect(url_for('views.home'))
 
     return render_template("sign_up.html", user=current_user)
